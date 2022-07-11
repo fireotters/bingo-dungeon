43554a0de621f8358e81799097d9edb3d131e17f
Rubén Bermejo Romero <mejoruben@gmail.com>
2022-07-12 00:15:39 +0200

    Linked Options menu with FMOD Mixer class to handle the volume of both buses

--- Assets/Scripts/Audio/FmodMixer.cs
+++ Assets/Scripts/Audio/FmodMixer.cs
@@ -1,31 +1,47 @@
-using System.Collections;
-using System.Collections.Generic;
+using System;
+using FMOD.Studio;
+using FMODUnity;
 using UnityEngine;
 
-public class FmodMixer : MonoBehaviour
+namespace Audio
 {
-    FMOD.Studio.Bus sfx;
-    FMOD.Studio.Bus music;
-    [SerializeField][Range(-80, 10f)] float sfxVolume;
-    [SerializeField][Range(-80, 10f)] float musicVolume;
-
-    void Start()
+    public class FmodMixer : MonoBehaviour
     {
-        sfx = FMODUnity.RuntimeManager.GetBus("bus:/Sfx");
-        music = FMODUnity.RuntimeManager.GetBus("bus:/Music");
-    }
+        private Bus sfx;
+        private Bus music;
+        [SerializeField] private StudioEventEmitter sfxDemoEmitter;
+        
+        private void Start()
+        {
+            sfx = RuntimeManager.GetBus("bus:/Sfx");
+            music = RuntimeManager.GetBus("bus:/Music");
+        }
 
+        public void ChangeMusicVolume(float dB)
+        {
+            print($"Set music decibels to {dB}");
+            music.setVolume(DecibelToLinear(dB));
+            PlayerPrefs.SetFloat("Music", dB);
+            PlayerPrefs.Save();
+        }
 
-    //Change parameters only when the audio slider changes
-    void Update()
-    {
-        sfx.setVolume(DecibelToLinear(sfxVolume));
-        music.setVolume(DecibelToLinear(musicVolume));
-    }
+        public void ChangeSfxVolume(float dB)
+        {
+            print($"Set sfx decibels to {dB}");
+            sfx.setVolume(DecibelToLinear(dB));
+            PlayerPrefs.SetFloat("SFX", dB);
+            PlayerPrefs.Save();
 
-    private float DecibelToLinear (float dB)
-    {
-        float linear = Mathf.Pow(10f, dB / 20f);
-        return linear;
+            if (!sfxDemoEmitter.IsPlaying())
+            {
+                sfxDemoEmitter.Play();
+            }
+        }
+
+        private float DecibelToLinear(float dB)
+        {
+            var linear = Mathf.Pow(10f, dB / 20f);
+            return linear;
+        }
     }
 }

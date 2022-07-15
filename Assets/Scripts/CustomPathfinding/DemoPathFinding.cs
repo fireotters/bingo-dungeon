eea7561a3bb3b80903da31738c7a8d62f12139ae
Rioni <nrosa01@ucm.es>
2022-07-15 23:44:51 +0200

    Added maximum range to pathfinding

--- Assets/Scripts/CustomPathfinding/DemoPathFinding.cs
+++ Assets/Scripts/CustomPathfinding/DemoPathFinding.cs
@@ -9,22 +9,29 @@ public class DemoPathFinding : MonoBehaviour
     public Transform endPos;
     public Transform startPos;
     public Tilemap tilemap;
+    public int range;
     [FormerlySerializedAs("linePath")] public LineRenderer lineRenderer;
 
     private void Update()
     {
         // if (Input.GetMouseButtonDown(0))
         // {
-            var mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-            mousePos.z = 0;
-            endPos.position = mousePos;
-            var linePath = AStar.FindFourDirectionLinePath(tilemap, startPos.position, endPos.position);
+        var mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        mousePos.z = 0;
+        endPos.position = mousePos;
+        var linePath = AStar.FindFourDirectionLinePath(tilemap, startPos.position, endPos.position);
 
-            if (linePath != null)
-            {
-                lineRenderer.positionCount = linePath.Length;
-                lineRenderer.SetPositions(linePath.nodes);
-            }
+        if (linePath != null && IsInRange(tilemap.WorldToCell(startPos.position), tilemap.WorldToCell(linePath.EndNode)))
+        {
+            lineRenderer.positionCount = linePath.Length;
+            lineRenderer.SetPositions(linePath.nodes);
+        }
         // }
+    }
+
+    bool IsInRange(Vector3Int startPos, Vector3Int endPos)
+    {
+        Vector3Int distance = endPos - startPos;
+        return distance.magnitude < (range + 1);
     }
 }

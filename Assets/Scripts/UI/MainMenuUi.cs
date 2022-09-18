81a9e7bd89604931ef3cbbe4181f0f423fcbc0bb
Cameron <cam.crosse97@outlook.com>
2022-09-18 23:15:42 +1000

    You thought I removed the Loss level ahahaha
    - I made it look better and actually functional and actually reachable by typing 'loss' on the main menu ahahaha

--- Assets/Scripts/UI/MainMenuUi.cs
+++ Assets/Scripts/UI/MainMenuUi.cs
@@ -3,6 +3,7 @@ using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using Signals;
+using System.Collections.Generic;
 
 namespace UI
 {
@@ -17,6 +18,10 @@ namespace UI
         [SerializeField] private TextMeshProUGUI versionText;
         [SerializeField] private GameObject desktopButtons, webButtons;
         private StudioEventEmitter menuSong;
+
+        // Easter Egg. Code from Jessespike on Unity Forums (http://answers.unity.com/answers/1241572/view.html)
+        const int MaxInputHistory = 8;
+        [HideInInspector] public List<string> inputHistory = new List<string>();
 
         private readonly CompositeDisposable _disposables = new();
 
@@ -44,6 +49,51 @@ namespace UI
             }
 
             SignalBus<SignalUiMainMenuStartGame>.Subscribe(StartGame).AddTo(_disposables);
+        }
+
+        private void Update()
+        {
+            UpdateEasterEggState();
+        }
+
+        private void UpdateEasterEggState()
+        {
+            if (!string.IsNullOrEmpty(Input.inputString))
+            {
+                inputHistory.Insert(0, Input.inputString);
+                while (inputHistory.Count > MaxInputHistory)
+                {
+                    inputHistory.RemoveAt(inputHistory.Count - 1);
+                }
+            }
+
+            if (CheckForKeyword("loss"))
+            {
+                Debug.Log("It's morbin' time");
+                inputHistory.Clear();
+                menuSong.Stop();
+                SceneManager.LoadScene($"Scenes/LevelScenes/ZZ Loss Secret (thx Tesla)");
+            }
+        }
+
+        private bool CheckForKeyword(string keyword)
+        {
+            if (inputHistory.Count >= keyword.Length)
+            {
+                for (int i = 0; i < keyword.Length; i++)
+                {
+                    if (keyword[keyword.Length - 1 - i].ToString() != inputHistory[i])
+                    {
+                        // input does not match keyword
+                        return false;
+                    }
+                }
+                // input and keyword match has been found
+                return true;
+            }
+
+            // not enough input to do comparison
+            return false;
         }
 
         private void SetVersionText()
